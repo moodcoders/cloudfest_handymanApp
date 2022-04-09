5f0b9e3b1df9aea06509e81f0d46e2c91cf1d7d1
mdghazanfar <ghazanfaralam642786@gmail.com>
2022-04-09 18:10:48 +0530

    remove unnecessary empty space

--- screens/BookingStatus/NewBooking.tsx
+++ screens/BookingStatus/NewBooking.tsx
@@ -2,82 +2,77 @@ import React from 'react';
 import { StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native';
 import { View, Text } from '../../components/Themed';
 
-
 import DataServices from '../../DataStore/DataService';
 
-
-
-const NewBooking = ({ navigation }: any) => {
+const NewBooking = () => {
     return (
-        <View >
-            <View style={styles.backgroundColor} lightColor="#edf1fb" darkColor="rgba(0, 0, 0, 0.22)">
-                <ScrollView>
-                    <View style={{ backgroundColor: 'transparent', }} >
-                        {DataServices.map((handyman, index) => {
-                            return (
-                                <View key={index} style={[styles.card, styles.shadow]} >
-                                    <View>
-                                        <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
-                                            <View>
-                                                <Text style={styles.bookingId}>Booking Number is </Text>
-                                            </View>
-                                            <View>
-                                                <Text style={{ fontSize: 12 }}>bg6789p0</Text>
-                                            </View>
+        <View style={styles.backgroundColor} lightColor="#edf1fb" darkColor="rgba(0, 0, 0, 0.22)">
+            <ScrollView>
+                <View style={{ backgroundColor: 'transparent', }} >
+                    {DataServices.map((handyman, index) => {
+                        return (
+                            <View key={index} style={[styles.card, styles.shadow]} >
+                                <View>
+                                    <View style={{ flexDirection: 'row', alignSelf: 'center' }}>
+                                        <View>
+                                            <Text style={styles.bookingId}>Booking Number is </Text>
                                         </View>
-                                        <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 5 }}>
-                                            <View>
-                                                <Image
-                                                    style={styles.image}
-                                                    resizeMode="cover"
-                                                    source={handyman.img}
-                                                />
+                                        <View>
+                                            <Text style={{ fontSize: 12 }}>bg6789p0</Text>
+                                        </View>
+                                    </View>
+                                    <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 5 }}>
+                                        <View>
+                                            <Image
+                                                style={styles.image}
+                                                resizeMode="cover"
+                                                source={handyman.img}
+                                            />
+                                        </View>
+                                        <View>
+                                            <Text style={styles.clientName}>
+                                                {handyman.name}
+                                            </Text>
+                                        </View>
+                                    </View>
+                                    <View style={{ marginLeft: 35 }}>
+                                        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                                            <View style={{ alignItems: 'center' }}>
+                                                <Text>4 pm </Text>
+                                                <Text>Monday,  Sep 7 </Text>
                                             </View>
-                                            <View>
-                                                <Text style={styles.clientName}>
-                                                    {handyman.name}
+                                            <View style={styles.verticleLine}></View>
+                                            <View style={{ width: '50%', }}>
+                                                <Text ellipsizeMode='tail' numberOfLines={2} style={styles.address}>
+                                                    DC-220, Newtown, Kolkata - 700156
                                                 </Text>
                                             </View>
                                         </View>
-                                        <View style={{ marginLeft: 35 }}>
-                                            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                                                <View style={{ alignItems: 'center' }}>
-                                                    <Text>4 pm </Text>
-                                                    <Text>Monday,  Sep 7 </Text>
-                                                </View>
-                                                <View style={styles.verticleLine}></View>
-                                                <View style={{ width: '50%', }}>
-                                                    <Text ellipsizeMode='tail' numberOfLines={2} style={styles.address}>
-                                                        DC-220, Newtown, Kolkata - 700156
-                                                    </Text>
-                                                </View>
+                                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10, alignItems: 'center' }}>
+                                            <View style={{ flexDirection: 'row' }}>
+                                                <Text style={{ fontWeight: 'bold' }}>Approx :</Text>
+                                                <Text>5 Km</Text>
                                             </View>
-                                            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10, alignItems: 'center' }}>
-                                                <View style={{ flexDirection: 'row' }}>
-                                                    <Text style={{ fontWeight: 'bold' }}>Approx :</Text>
-                                                    <Text>5 Km</Text>
-                                                </View>
-                                                <View style={{ flexDirection: "row" }} >
-                                                    <TouchableOpacity
-                                                        style={styles.serviceBtn}
-                                                    >
-                                                        <Text style={styles.btnName}>ACCEPT</Text>
-                                                    </TouchableOpacity>
-                                                    <TouchableOpacity
-                                                        style={[styles.serviceBtn, { backgroundColor: 'red', }]}>
-                                                        <Text style={[styles.btnName,]}>REJECT</Text>
-                                                    </TouchableOpacity>
-                                                </View>
+                                            <View style={{ flexDirection: "row" }} >
+                                                <TouchableOpacity
+                                                    style={styles.serviceBtn}
+                                                >
+                                                    <Text style={styles.btnName}>ACCEPT</Text>
+                                                </TouchableOpacity>
+                                                <TouchableOpacity
+                                                    style={[styles.serviceBtn, { backgroundColor: 'red', }]}>
+                                                    <Text style={[styles.btnName,]}>REJECT</Text>
+                                                </TouchableOpacity>
                                             </View>
                                         </View>
                                     </View>
                                 </View>
-                            );
-                        })}
-                    </View>
-                </ScrollView>
-            </View>
-        </View >
+                            </View>
+                        );
+                    })}
+                </View>
+            </ScrollView>
+        </View>
     );
 };
 
