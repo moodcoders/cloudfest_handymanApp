a69f2871770e9ec1f68d919e4c0bba3a4c0c6bc6
osama bin junaid <osama.junaid@c3m.io>
2022-05-02 20:02:33 +0530

    connected cloud run instance of auth microservice

--- services/otp.tsx
+++ services/otp.tsx
@@ -1,4 +1,4 @@
-const AUTH_API = 'http://192.168.1.11:4000';
+const AUTH_API = 'https://auth-service-ffptpmrzya-el.a.run.app';
 import { notifyMessage } from '../constants/NotifyMessage';
 
 /**generateOtpAPI is generating OTP for the handyman */
